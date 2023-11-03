99afb2a70441cc37c480694dccbf2ec1fa8696f8
Ugur Cenar <cenar93@gmail.com>
2023-11-03 13:50:47 +0100

    Legg til SøktAvType

--- src/main/kotlin/no/nav/bidrag/domene/enums/SøktAvType.kt
+++ src/main/kotlin/no/nav/bidrag/domene/enums/SøktAvType.kt
@@ -4,18 +4,18 @@ enum class SøktAvType(val kode: String) {
     BIDRAGSMOTTAKER("MO"),
     BIDRAGSPLIKTIG("PL"),
     BARN_18_AAR("BB"),
-
     BM_I_ANNEN_SAK("AS"),
     NAV_BIDRAG("ET"), // TK
     FYLKESNEMDA("FN"),
     NAV_INTERNASJONALT("IN"),
     KOMMUNE("KU"),
-    KONVERTERING("KV"), // Trenger vi dette?
     NORSKE_MYNDIGHET("NM"),
     UTENLANDSKE_MYNDIGHET("UM"),
     VERGE("VE"),
     TRYGDEETATEN_INNKREVING("TI"),
-    KLAGE_ANKE("FK"); // FTK
+    KLAGE_ANKE("FK"), // FTK
+
+    KONVERTERING("KV"); // Kodeverdi som brukes i eldre overførte saker i Bisys. Ikke bruk
 
     companion object {
         fun fraKode(kode: String): SøktAvType? = entries.find { it.kode == kode }
