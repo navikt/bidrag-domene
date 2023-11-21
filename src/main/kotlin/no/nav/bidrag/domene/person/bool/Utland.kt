b6f433230c16f753eba9d80a9fb549e865a01ae2
Ugur Cenar <cenar93@gmail.com>
2023-11-21 13:18:32 +0100

    Slettet deprecated klasser og enumer

--- src/main/kotlin/no/nav/bidrag/domene/person/bool/Utland.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("unused")
-
-package no.nav.bidrag.domene.person.bool
-
-import no.nav.bidrag.domene.felles.Verdiobjekt
-
-@Deprecated("Bruk boolean i stedet")
-class Utland(override val verdi: Boolean) : Verdiobjekt<Boolean>()
--- src/main/kotlin/no/nav/bidrag/domene/sak/bool/Ukjent.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("unused")
-
-package no.nav.bidrag.domene.sak.bool
-
-import no.nav.bidrag.domene.felles.Verdiobjekt
-
-@Deprecated("Bruk boolean i stedet")
-class Ukjent(override val verdi: Boolean) : Verdiobjekt<Boolean>()
